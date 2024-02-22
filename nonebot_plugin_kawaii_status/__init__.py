bbcfff3e65ab4bf57de4c26e4e6e75e944c0814a
KomoriDev <mute231010@gmail.com>
2024-02-22 16:13:04 +0800

    :bookmark: v0.1.1

--- nonebot_plugin_kawaii_status/__init__.py
+++ nonebot_plugin_kawaii_status/__init__.py
@@ -1,19 +1,17 @@
 """运行状态"""
 
-from nonebot import require
 from nonebot.rule import Rule
-from nonebot import get_plugin_config
 from nonebot.permission import SUPERUSER
 from nonebot.plugin import PluginMetadata
+from nonebot import require, get_plugin_config
 
 require("nonebot_plugin_alconna")
-from nonebot_plugin_alconna import Command
-from nonebot_plugin_alconna import __plugin_meta__
 from nonebot_plugin_alconna.uniseg import UniMessage
+from nonebot_plugin_alconna import Command, __plugin_meta__
 
 from .config import Config
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __plugin_meta__ = PluginMetadata(
     name="运行状态",
     description="NoneBot2 服务器状态查看插件",
@@ -34,15 +32,18 @@ from .drawer import draw
 
 config = get_plugin_config(Config).status
 
+
 def to_me() -> Rule:
     if config.to_me:
         from nonebot.rule import to_me
+
         return to_me()
 
     async def _to_me() -> bool:
         return True
 
     return Rule(_to_me)
+
 
 status = (
     Command("status", help_text="查看林汐运行状态")
@@ -55,4 +56,3 @@ status = (
         permission=SUPERUSER if config.only_superuser else None,
     )
 )
-
